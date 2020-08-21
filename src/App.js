cc8184ab84d642e530a7d5cdfcfe7440d462a776
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-22 03:03:03 +0500

    46 - Method reference (function, values)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Person               from "./Person/Person";
+import Person              from "./Person/Person";
 import "./App.css";
 
 const App = props => {
-    const [ personsState, setPersonsState] = useState({
+    const [personsState, setPersonsState] = useState({
         persons: [
             { name: "Max", "age": 28 },
             { name: "Alex", "age": 29 },
@@ -11,17 +11,17 @@ const App = props => {
         ],
     });
 
-    const switchNameHandler = () => {
+    const switchNameHandler = ( e, newName = "Maximillianus" ) => {
         // DON'T DO THIS: this.state.persons[0].name = "Maximilian";
         setPersonsState({
             ...personsState,
             persons: [
-                { name: "Maximilian", "age": 28 },
+                { name: newName, "age": 28 },
                 { name: "Alex", "age": 29 },
                 { name: "Nick", "age": 25 },
             ],
 
-        })
+        });
     };
 
     return (
@@ -29,11 +29,20 @@ const App = props => {
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
             <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+            <Person
+                click={switchNameHandler.bind(null, null, "Maxi")}
+                name={personsState.persons[0].name}
+                age={personsState.persons[0].age} />
+            <Person
+                click={switchNameHandler.bind(null, null, "Moxi")}
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age} />
+            <Person
+                click={switchNameHandler}
+                name={personsState.persons[2].name}
+                age={personsState.persons[2].age} />
         </div>
     );
-}
+};
 
 export default App;
