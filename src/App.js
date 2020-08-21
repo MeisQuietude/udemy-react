c16bc5f3a785fcd654c52f7ad0b0c21280b3855b
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-22 01:03:01 +0500

    [IMP] 30 - using React.createElement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App!</h1>
-            </div>
+            // <div className="App">
+            //     <h1>Hi, I'm a React App!</h1>
+            // </div>
+            React.createElement("div", { className: "App" }), React.createElement("h1", null, "I'm a React App!")
         );
     }
 }
