11cbb7832e23a797475a5fd3cc8be044d909f0e4
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-22 18:38:34 +0500

    [IMP] 66-67 - Setting classes dinamicaly

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ class App extends Component {
 
     render() {
         const styleButton = {
+            backgroundColor: "green",
+            color: "white",
             font: "inherit",
             border: "1px solid blue",
             padding: "8px",
@@ -56,14 +58,22 @@ class App extends Component {
                             key={index} />;
                     })}
                 </div>
-
             );
+            styleButton.backgroundColor = "red";
+        }
+
+        const classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red');
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push('bold');
         }
 
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
+                <p className={classes.join(" ")}>This is really working!</p>
                 <button
                     style={styleButton}
                     onClick={this.togglePersonHandler}>Toggle Persons
