831f7ac4a78121c5182c3e5fff96177f0ddd29a7
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-22 16:37:27 +0500

    [IMP] 54 - Handling dynamic content; also - use app as a container component instead of a functional

--- src/App.js
+++ src/App.js
@@ -1,84 +1,72 @@
-import React, { useState } from "react";
-import Person              from "./Person/Person";
+import React, { Component } from "react";
+import Person               from "./Person/Person";
 import "./App.css";
 
-const App = props => {
-    const [personsState, setPersonsState] = useState({
+class App extends Component {
+    state = {
         persons: [
             { name: "Max", "age": 28 },
             { name: "Alex", "age": 29 },
             { name: "Nick", "age": 26 },
         ],
         showPersons: true,
-    });
+    };
 
-    const switchNameHandler = ( e, newName = "Maximillianus" ) => {
-        // DON'T DO THIS: this.state.persons[0].name = "Maximilian";
-        setPersonsState({
-            ...personsState,
+    nameChangedHandler = ( event ) => {
+        this.setState({
             persons: [
-                { name: newName, "age": 28 },
-                { name: "Alex", "age": 29 },
-                { name: "Nick", "age": 25 },
+                { name: "Max", age: 28 },
+                { name: event.target.value, age: 29 },
+                { name: "Nick", age: 26 },
             ],
 
         });
     };
 
-    const nameChangedHandler = ( e ) => {
-        setPersonsState({
-            ...personsState,
-            persons: [
-                { name: "Max", "age": 28 },
-                { name: e.target.value, "age": 29 },
-                { name: "Nick", "age": 26 },
-            ],
-
+    togglePersonHandler = ( event ) => {
+        this.setState({
+            showPersons: !this.state.showPersons,
         });
     };
 
-    const togglePersonHandler = ( e ) => {
-        setPersonsState({
-            ...personsState,
-            showPersons: !personsState.showPersons,
-        });
-    };
+    render() {
+        const styleButton = {
+            font: "inherit",
+            border: "1px solid blue",
+            padding: "8px",
+            cursor: "pointer",
+        };
 
-    const styleButton = {
-        font: "inherit",
-        border: "1px solid blue",
-        padding: "8px",
-        cursor: "pointer",
-    };
+        let persons = null;
+        if ( this.state.showPersons ) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age} />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age} />
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age} />
+                </div>
 
-    return (
-        <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <p>This is really working!</p>
-            <button
-                style={styleButton}
-                onClick={togglePersonHandler}>Toggle Persons
-            </button>
-            {
-                personsState.showPersons ?
-                    <div>
-                        <Person
-                            click={switchNameHandler.bind(null, null, "Maxi")}
-                            name={personsState.persons[0].name}
-                            age={personsState.persons[0].age} />
-                        <Person
-                            click={switchNameHandler.bind(null, null, "Moxi")}
-                            changed={nameChangedHandler}
-                            name={personsState.persons[1].name}
-                            age={personsState.persons[1].age} />
-                        <Person
-                            click={switchNameHandler}
-                            name={personsState.persons[2].name}
-                            age={personsState.persons[2].age} />
-                    </div> : null
-            }
-        </div>
-    );
-};
+            );
+        }
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button
+                    style={styleButton}
+                    onClick={this.togglePersonHandler}>Toggle Persons
+                </button>
+                {persons}
+            </div>
+        );
+    }
+}
 
 export default App;
